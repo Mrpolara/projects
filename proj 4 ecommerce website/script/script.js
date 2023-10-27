c4ef805e2ae162b0507f01a546a8b638a8aed899
Mrpolara <dhruvpolara0808203@gmail.com>
2023-10-27 12:34:17 +0530

    reload

--- proj 4 ecommerce website/script/script.js
+++ proj 4 ecommerce website/script/script.js
@@ -5,25 +5,25 @@ function addOrRemove(price) {
     if (isTrue) total += price;
     else total -= price;
   }
-  
+
   if (price == 200) {
     let isTrue = document.getElementById("nintendo").checked;
     if (isTrue) total += price;
     else total -= price;
   }
-  
+
   if (price == 300) {
     let isTrue = document.getElementById("vr").checked;
     if (isTrue) total += price;
     else total -= price;
   }
-  
+
   if (price == 400) {
     let isTrue = document.getElementById("playstaion").checked;
     if (isTrue) total += price;
     else total -= price;
   }
-  
+
   if (price == 500) {
     let isTrue = document.getElementById("xbox").checked;
     if (isTrue) total += price;
@@ -32,12 +32,22 @@ function addOrRemove(price) {
   console.log(total);
   document.getElementById("amountText").innerHTML = total;
 
-  let discount = "(" + (12*total)/100 + ")";
+  let discount = "(" + (12 * total) / 100 + ")";
   document.getElementById("discountText").innerHTML = discount;
 
-  let final = total-(12*total)/100;
+  let final = total - (12 * total) / 100;
   document.getElementById("totalPayableAmountText").innerHTML = final;
-  
-  
-  
 }
+
+// function resetBtn() {
+
+//   total = 0;
+//   console.log("hello");
+//   document.getElementById("amountText").innerHTML = " ";
+//   document.getElementById("discountText").innerHTML = " ";
+//   document.getElementById("totalPayableAmountText").innerHTML = " ";  
+// }
+
+document.getElementById("resetBtn").addEventListener("click", function () {
+  location.reload();
+});
